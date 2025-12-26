8b3a8438095d07788c316c7b314533c25b79eae9
evrahimkhan <88596341+evrahimkhan@users.noreply.github.com>
2025-12-26 16:31:10 +0600

    Modify start.sh for Arch Linux compatibility
    Updated commands and paths for Arch Linux compatibility.

--- start.sh
+++ start.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# Start DBus
-mkdir -p /var/run/dbus
+set -e
+
+# Start DBus (Arch way)
+mkdir -p /run/dbus
 dbus-daemon --system --fork
 
-# Start SSH
-service ssh start
+# Start SSH (Arch uses sshd, not "service ssh")
+/usr/bin/sshd
 
 # Prepare X11 directory
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
 # Start XRDP
-/usr/sbin/xrdp --nodaemon
+/usr/bin/xrdp --nodaemon
